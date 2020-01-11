1e913f7b44ee0dff0d73b5c133dd2606d3ea7ac8
arjunds <arjundsampath@gmail.com>
2020-01-10 19:22:21 -0800

    Changed default max value for HSV slider

--- color_filter_tuner.py
+++ color_filter_tuner.py
@@ -26,9 +26,9 @@ cv2.namedWindow("Tuner")
 cv2.createTrackbar('Min Hue','Tuner',0,255,nothing)
 cv2.createTrackbar('Max Hue','Tuner',0,255,nothing)
 cv2.createTrackbar('Min Saturation','Tuner',0,255,nothing)
-cv2.createTrackbar('Max Saturation','Tuner',0,255,nothing)
-cv2.createTrackbar('Min Value','Tuner',0,255,nothing)
-cv2.createTrackbar('Max Value','Tuner',0,255,nothing)
+cv2.createTrackbar('Max Saturation','Tuner',255,255,nothing)
+cv2.createTrackbar('Min Value','Tuner',255,255,nothing)
+cv2.createTrackbar('Max Value','Tuner',255,255,nothing)
 min_h = 0
 min_s = 0
 min_v = 0
@@ -39,7 +39,7 @@ frame_count = 0
 width, height = 600, 400
 
 #Sets up video stream to be used in while loop
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
@@ -48,11 +48,6 @@ display = cap.read()[1]
 while True:
     frame = cap.read()[1]
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv_frame, (min_h, min_s, min_v), (max_h, max_s, max_v))
-    cv2.imshow("Tuner", display)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
     min_h = cv2.getTrackbarPos("Min Hue", "Tuner")
     max_h = cv2.getTrackbarPos("Max Hue", "Tuner")
     min_s = cv2.getTrackbarPos("Min Saturation", "Tuner")
@@ -60,6 +55,11 @@ while True:
     min_v = cv2.getTrackbarPos("Min Value", "Tuner")
     max_v = cv2.getTrackbarPos("Max Value", "Tuner")
     #low_value = (min_h, min_s, min_v)
+    mask = cv2.inRange(hsv_frame, (min_h, min_s, min_v), (max_h, max_s, max_v))
+    cv2.imshow("Tuner", display)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
     #high_value = (max_h, max_s, max_v)
     
     #This combines the mask with the original image
